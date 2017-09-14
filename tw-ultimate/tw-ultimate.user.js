ebb4f08be06e275fec5381a993362c97debe4474
SchTomi <sch.tom93@gmail.com>
2017-09-14 18:29:47 +0200

    Fix Costum CSS problems

--- tw-ultimate/tw-ultimate.user.js
+++ tw-ultimate/tw-ultimate.user.js
@@ -58,7 +58,7 @@
          static
          import () {
              let link = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/siimple/2.0/siimple.min.css">
-        <link rel="stylesheet" href="https://raw.githubusercontent.com/Sch-Tomi/tw-scripts/master/tw-ultimate/assets/css/flexboxgrid.css" type="text/css" >`
+        <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/Sch-Tomi/tw-scripts/master/tw-ultimate/assets/css/flexboxgrid.css">`
 
              $('head').append(link)
          }
